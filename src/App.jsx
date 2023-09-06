4448e9f72b2c61732328dca614c81a611866bd28
nova vita <42131370+josephc1417@users.noreply.github.com>
2023-09-06 04:29:52 -0400

    Working on fetch data- needed to refresh on API's

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import Starter from './tutorial/02-useEffect/starter/03-multiple-effects'
+
+
+
+function App() {
+  return (
+    <div className='container'>
+      <h2>
+        <Starter />
+      </h2>
+    </div>
+  );
+}
+
+export default App;
