b1659b5dc7ab4774dc3edbbffb890ff672317835
nova vita <42131370+josephc1417@users.noreply.github.com>
2023-10-11 12:29:30 -0400

    Functional component wasn't rendering

--- src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -4,46 +4,39 @@ import { useReducer } from 'react';
 
 
 
-// ● (1) - defaultState = lim f(x) = ∞  ● 
+
 const defaultState = {
   people: data,
   isLoading:false,
 };
 
 
-// ● (2) - reducer function ● 
+
 const reducer = (state,action) => {
   if(action.type === 'CLEAR_LIST') {
     return {...state, people: []};
   }
 }
 
-
 const ReducerBasics = () => {
+  const [state, dispatch] = useReducer(reducer, defaultState);
 
-// ● (3) - useReducer Hook ●   
-const { state, dispatch } = useReducer(reducer, defaultState);
-
-
-// • (x)removeItem (id) • 
-const removeItem = (id) => {
-  // let newPeople = people.filter((person) => person.id !== id);
-  // setPeople(newPeople);
-};
-
-// • (x)clearList {type: 'CLEAR_LIST'} • 
-const clearList = () => {
+  const removeItem = (id) => {
+    // let newPeople = people.filter((person) => person.id !== id);
+    // setPeople(newPeople);
+  };
+ 
+ 
+ 
+  const clearList = () => {
   dispatch({ type: 'CLEAR_LIST' });   
-}
-
-// • (x)resetList • 
+  }  
+  
   const resetList = () => {
   }
-
-console.log('====================================');
-console.log(state);
-console.log('====================================');
-
+  
+  
+  console.log(state);
   return (
     <div>
       {state.people.map((person) => {
@@ -51,11 +44,12 @@ console.log('====================================');
         return (
           <div key={id} className='item'>
             <h4>{name}</h4>
-            <button onClick={() => removeItem(id)}>remove</button>
+            <button onClick={() => removeItem(id)}>remove
+            </button>
           </div>
         );
       })}
-      {state.people.length < 1 ?(
+      {state.people.length < 1 ? (
         <button
         className='btn'
         style={{ marginTop: '2rem' }}
@@ -63,11 +57,11 @@ console.log('====================================');
       >
         Reset List
       </button>
-      ):(
+      ) : (
         <button
         className='btn'
         style={{ marginTop: '2rem' }}
-        onClick={() => setPeople([])}
+        onClick={clearList}
       >
         clear items
       </button>
